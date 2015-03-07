81a4296900a73d1cbe611ee555891d74e551b458
twer <twer@CNtwer-9.local>
2015-03-07 09:39:01 +0530

    [hj] #n/a should notify fans as well when footballgame goal, add notify fans test in footballgame test

--- baseline/src/footballgame/FootballGame.java
+++ baseline/src/footballgame/FootballGame.java
@@ -5,12 +5,18 @@ package footballgame;
  */
 public class FootballGame {
     private Reporter reporter;
+    private Fans fans;
 
     public void add(Reporter reporter) {
         this.reporter = reporter;
     }
 
+    public void add(Fans fans) {
+        this.fans = fans;
+    }
+
     public void goal(String scoringTeam) {
         this.reporter.reactToGoal(scoringTeam);
+        this.fans.reactToGoal(scoringTeam);
     }
 }
--- baseline/test/footballgame/FootballGameTest.java
+++ baseline/test/footballgame/FootballGameTest.java
@@ -14,17 +14,26 @@ public class FootballGameTest {
 
     private FootballGame footballGame;
     private Reporter reporter;
+    private Fans fans;
 
     @Before
     public void Setup() throws Exception {
         footballGame = new FootballGame();
         reporter = mock(Reporter.class);
+        fans = mock(Fans.class);
         footballGame.add(reporter);
+        footballGame.add(fans);
     }
 
     @Test
     public void shouldNotifyReporterOfGoal() {
         footballGame.goal("Team A");
         verify(reporter).reactToGoal("Team A");
+    }
+
+    @Test
+    public void shouldNotifyFansOfGoal() {
+        footballGame.goal("Team A");
+        verify(fans).reactToGoal("Team A");
     }
 }
